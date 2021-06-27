91269b84a3c0c99a5c47dd1bb6346ca423e169c0
Mateus Lemos <lemonsmateus@gmail.com>
2021-06-27 18:05:21 -0300

    Add files via upload
    initial commit

---
+++ mrbi.java
@@ -0,0 +1,13 @@
+package miniruby;
+
+public class mrbi {
+    public static void main(String args[]) {
+        if (args.length != 1)
+        {
+            System.out.println("Uso: java mrbi [arquivo fonte]");
+            return;
+        }
+
+
+    }
+}
